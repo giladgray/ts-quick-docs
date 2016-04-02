df3c0fab5ca1ee2691bb6b767b6c13188d8f4dbc
Gilad Gray <ggray@palantir.com>
2016-04-01 17:59:16 -0700

    npm scripts and a test!

---
+++ test/index.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/main.d.ts"/>
+
+import * as assert from "assert";
+
+import { Documentation } from "../src/documentation";
+
+describe("Documentation", () => {
+    it("works", () => {
+        assert.ok(true);
+    });
+});
