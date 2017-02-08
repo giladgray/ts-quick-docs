1c59fc1b5d2e325651f2ca126e2eb31b1c706b40
Gilad Gray <ggray@palantir.com>
2017-02-07 18:03:20 -0800

    more accurate interfaces - `IDocEntry` is fully required - add docs for common JSDoc tags - remove unused `inheritedFrom`

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,20 +4,27 @@
  * Arbitrary camelCased tags are supported via index signature.
  */
 export interface IJsDocTags {
+    /** default value of this entry. */
     default?: string;
-    deprecated?: true;
+
+    /** whether this entry is deprecated. string value is the reason. */
+    deprecated?: string | true;
+
+    /** whether this entry is internal to the typescript project. */
     internal?: true;
+
+    /** version in which this entry was introduced. */
     since?: string;
-    [tag: string]: string | true;
+
+    [tag: string]: string | true | undefined;
 }
 
 export interface IDocEntry {
-    documentation?: string;
-    fileName?: string;
-    inheritedFrom?: string;
-    name?: string;
+    documentation: string;
+    fileName: string;
+    name: string;
     tags: IJsDocTags;
-    type?: string;
+    type: string;
 };
 
 export interface IPropertyEntry extends IDocEntry {
